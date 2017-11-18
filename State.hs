fe6faa481ace6d6ce47e5938365342681ba1eb5a
Mikael Blomstrand <chawlindel@gmail.com>
2017-11-18 22:30:35 +0100

    Implemented Monad.

--- State.hs
+++ State.hs
@@ -8,6 +8,15 @@ newtype State s a = State { runState :: (s -> (s,a)) }
 get :: State s s
 get = State $ \x-> (x,x)
 
+put :: s -> State s ()
+put s = State $ const (s,())
+
+mkState :: s -> a -> State s a
+mkState s a = State $ \_ -> (s,a)
+
+unsafeRun :: State s a -> (s,a)
+unsafeRun state = runState state undefined
+
 instance Functor (State s) where
     fmap f state = State $ \x-> case runState state x of
         (state, val) -> (state, f val)
@@ -17,3 +26,8 @@ instance Applicative (State s) where
     f <*> x = State $ \s -> case runState f s of
         (s,f) -> case runState x s of
             (s,x) -> (s,f x)
+
+instance Monad (State s) where
+    x >>= f = State $ \s -> case runState x s of
+        (s,x) -> runState (f x) s
+        
