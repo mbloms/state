fc4f4811bfcc0f2055881ba60371ad9cf2c200f3
Mikael Blomstrand <chawlindel@gmail.com>
2017-11-19 14:37:49 +0100

    Lift and test.

--- StateT.hs
+++ StateT.hs
@@ -1,8 +1,14 @@
 module Control.Monad.State.StateT where
 
-import State
 import Control.Monad
 import Control.Applicative
+import Data.Array.MArray
+--import Data.Maybe 
+--import Data.Array.ST 
+--import Control.Monad.ST 
+import Data.Array.IO 
+import Data.Array
+
 
 newtype StateT m s a = StateT {runStateT :: (s -> m (s,a))}
 
@@ -19,3 +25,20 @@ instance Monad m => Monad (StateT m s) where
     (StateT x) >>= f = StateT $ \s -> do
         (state, val) <- x s
         runStateT (f val) state
+
+lift :: Monad m => m a -> StateT m s a
+lift c = StateT $ \s -> c >>= (\x -> return (s,x))
+
+liftF :: Monad m => (a -> m b) -> a -> StateT m s b
+liftF f x = lift (f x)
+
+get :: Monad m => StateT m s s
+get = StateT $ \x-> return (x,x)
+
+put :: Monad m => s -> StateT m s ()
+put s = StateT . const $ return (s,())
+
+update :: (MArray a e m, Ix i) => i -> e -> StateT m (a i e) ()
+update i e = StateT $ \arr -> writeArray arr i e >> return (arr,())
+
+test = fmap snd (runStateT (lift (newArray (0,10) 0) >>= put >> update 0 5 >> get >>= liftF (freeze :: IOArray Int Int -> IO (Array Int Int))) (undefined :: IOArray Int Int))
