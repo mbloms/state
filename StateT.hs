40d6ef45afeb9980aebb903280041eb9c51e3425
Mikael Blomstrand <chawlindel@gmail.com>
2017-11-19 04:04:00 +0100

    State monad transformer.

--- StateT.hs
+++ StateT.hs
@@ -4,18 +4,18 @@ import State
 import Control.Monad
 import Control.Applicative
 
-newtype StateT m s a = StateT {runStateT :: m (State s a)}
+newtype StateT m s a = StateT {runStateT :: (s -> m (s,a))}
 
 instance Monad m => Functor (StateT m s) where
-    fmap f = StateT . (fmap . fmap) f . runStateT
+    fmap f state = StateT $ \x-> do 
+        (state, val) <- runStateT state x
+        return (state, f val)
 
 instance Monad m => Applicative (StateT m s) where
-    pure = StateT . return . return
-    --(<*>) = ap
+    pure x = StateT $ \s -> return (s,x)
+    (<*>) = ap
 
 instance Monad m => Monad (StateT m s) where
-    x >>= f = join $ StateT $ do
-            mstate <- runStateT x
-            return $ State $ \s->
-                case runState mstate s of
-                    (s,x') -> (s, f x')
+    (StateT x) >>= f = StateT $ \s -> do
+        (state, val) <- x s
+        runStateT (f val) state
